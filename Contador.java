586701ff4b84540d2cda9f17ffccb11075f6a6cd
Andriele Lange <andrielebarcelange@gmail.com>
2024-01-10 10:03:59 -0300

    concluído

--- Contador.java
+++ Contador.java
@@ -12,15 +12,15 @@ public class Contador{
 			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
 		
-		}catch (? exception) {
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+		}catch (ParametrosInvalidosException e) {
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
 		}
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
         if(parametroUm > parametroDois){
-            throw new ParametrosInvalidosException();
+            throw new ParametrosInvalidosException(parametroUm, parametroDois);
         }
 		
 		int contagem = parametroDois - parametroUm;
--- ParametrosInvalidosException.java
+++ ParametrosInvalidosException.java
@@ -1,3 +1,17 @@
 public class ParametrosInvalidosException extends Exception{
+    private int arg1;
+    private int arg2;
+
+    public ParametrosInvalidosException(int arg1, int arg2) {
+        this.arg1 = arg1;
+        this.arg2 = arg2;
+    }
+
+    @Override
+    public String toString() {
+        return "ParametrosInvalidosException: o número " + arg1 + " é maior que " + arg2;
+    }
+
+    
     
 }
